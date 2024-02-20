12b77db38aef9946d057bdbd200476a2a5d40169
eka <21106050023@student.uin-suka.ac.id>
2024-02-21 06:33:04 +0700

    feat: update latihan1

--- src/main/kotlin/com/dicoding/exam/latihan1/App.kt
+++ src/main/kotlin/com/dicoding/exam/latihan1/App.kt
@@ -17,14 +17,14 @@ package com.dicoding.exam.latihan1
  *  Sesuaikan fungsi di bawah ini dengan kode untuk mengecek apakah parameter number
  *  merupakan angka genap
  */
-fun isEvenNumber(number: Int) = false
+fun isEvenNumber(number: Int) = number % 2 == 0
 
 /**
  *  TODO 2
  *  Sesuaikan fungsi di bawah ini dengan kode untuk mengecek apakah parameter number
  *  lebih dari 5
  */
-fun moreThanFive(number: Int) = false
+fun moreThanFive(number: Int) = number > 5
 
 /**
  *  TODO 3
@@ -33,7 +33,7 @@ fun moreThanFive(number: Int) = false
  *
  *      param * ( param + 10 )
  */
-fun result(number: Int) = 0
+fun result(number: Int) = number * (number + 10)
 
 fun main() {
     val listNumber = 1.rangeTo(100)
